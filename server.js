14411ea417582ffe42983c82064bd2c2464f4554
Michał Skorupa <michal.skorupa@schibsted.com>
2021-08-13 11:05:20 +0200

    PSI test

--- server.js
+++ server.js
@@ -11,28 +11,56 @@ const basicAuth = require('express-basic-auth');
 
 console.log('Starting server...');
 (async () => {
-  const app = express();
-  const {app: lhciApp} = await createApp({
-    storage: {
-      storageMethod: 'sql',
-      sqlDialect: 'postgres',
-      ...(process.env.DATABSE_SSL === "false" ? {} : {
-        sqlConnectionSsl: true,
-        sqlDialectOptions: { ssl: {
-            require: true,
-            rejectUnauthorized: false
-          }},
-      }),
-      sqlConnectionUrl: process.env.DATABASE_URL,
-    },
-  });
+    const app = express();
+    const {app: lhciApp} = await createApp({
+        psiCollectCron: {
+            psiApiKey: process.env.PSI_API_KEY,
+            sites: [
+                /*
+                  {
+                  label: 'Production',
+                  projectSlug: 'the-project',
+                  schedule: '0 * * * *', // at the top of the hour, every hour
+                  numberOfRuns: 5,
+                  urls: ['http://example.com', 'http://example.com/pricing'],
+                },
 
-  app.use(basicAuth({
-    challenge: true,
-    users: { 'admin': process.env.ADMIN_PASS }
-  }))
-  app.use(lhciApp);
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Listening on port', process.env.PORT || 3000)
-  });
+                 */
+                {
+                    label: 'Sweship Development',
+                    branch: 'develop',
+                    projectSlug: 'sweship-web',
+                    schedule: '*/10 * * * *', // every 10 minutes
+                    numberOfRuns: 3,
+                    urls: [
+                        'https://sweship.seals.schibsted.pl',
+                        'https://sweship.seals.schibsted.pl/about-sweship'
+                    ],
+                },
+            ],
+        },
+        storage: {
+            storageMethod: 'sql',
+            sqlDialect: 'postgres',
+            ...(process.env.DATABSE_SSL === "false" ? {} : {
+                sqlConnectionSsl: true,
+                sqlDialectOptions: {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false
+                    }
+                },
+            }),
+            sqlConnectionUrl: process.env.DATABASE_URL,
+        },
+    });
+
+    app.use(basicAuth({
+        challenge: true,
+        users: {'admin': process.env.ADMIN_PASS}
+    }))
+    app.use(lhciApp);
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('Listening on port', process.env.PORT || 3000)
+    });
 })();
